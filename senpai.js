37493db392d97f76458b293c7367daac535ea164
Nick Walker <nick.walker@vendhq.com>
2015-07-14 00:09:40 +1200

    ihavenoideawhatimdoing.gif

--- senpai.js
+++ senpai.js
@@ -1,5 +1,6 @@
 var https = require('https');
 var Slack = require('slack-client');
+var fs = require('fs');
 
 var token = process.env.SLACK_TOKEN;
 var autoReconnect = true;
@@ -22,10 +23,41 @@ var checkUserData = function() {
 };
 
 var evalStory = function(username, data) {
+	console.log("During eval it is" + userData);
 	//console.log(data);
-	var values = JSON.parse(data);
-	console.log(values);
-	writeToChannel('I got ' + values.length + ' stories for ' + username);
+	var stories = JSON.parse(data);
+	//writeToChannel('I got ' + stories.length + ' stories for ' + username);
+	// Iterate over the parent stories where type = media_story
+	stories = stories.filter(function(story) {
+		return story["story_type"] == "media_story";
+	});
+
+	// Select the parent where the child story has a higher ID than the last seen for that user
+	var lastSeen = userData[username];
+	if (lastSeen === undefined) {
+		lastSeen = 0;
+	}
+
+	// Check all the stories to see if there is a substory with one with a higher value?
+	stories = stories.filter(function(story) {
+		if (story["substories"].length === 0) {
+			return false;
+		}
+
+		return story["substories"].some(function(value, index, array) {
+			return value["id"] > lastSeen;
+		});
+	});
+
+	if (stories.length > 0) {
+		// First member of array will have the most recent story!
+		var recent = stories[0];
+		//writeToChannel("Looks like " + username + " just watched " + recent.media.title);
+		// Echo out the data
+		console.log("Set user data for " + username);
+		userData[username] = recent.id;
+		console.log(userData);
+	}
 };
 
 var checkStory = function(username, callback) {
@@ -51,11 +83,27 @@ var checkStory = function(username, callback) {
 	}).end();
 };
 
+var writeUserData = function() {
+	console.log("Attempting to save: " + JSON.stringify(userData));
+	fs.writeFile('data.json', JSON.stringify(userData), function (err) {
+	if (err) throw err;
+	console.log('It\'s saved!');
+	});
+};
+
+var readUserData = function() {
+	fs.readFile('data.json', function (err, data) {
+	if (err) throw err;
+	userData = JSON.parse(data);
+	});
+};
+
 slack.on('open', function () {
 	console.log("Connection established!");
 	var channel = slack.getChannelByName('#anime');
 	var general = slack.getChannelByName('#general');
-	setTimeout(checkUserData, 10000);
+	setInterval(checkUserData, 5000);
+	setInterval(writeUserData, 7000);
 });
 
 slack.on('message', function() {
