bafebcbd57c8becaf9d07376ca1b5c9475e52dfe
Nick Walker <nick.walker@vendhq.com>
2015-07-13 23:26:06 +1200

    Add some hummingbird logic for testing

--- senpai.js
+++ senpai.js
@@ -6,11 +6,56 @@ var autoReconnect = true;
 var autoMark = true;
 
 var slack = new Slack(token, autoReconnect, autoMark);
+var channel = null;
+
+var userData = [];
+
+var writeToChannel = function(message) {
+	var channel = slack.getChannelByName('#anime');
+	channel.send(message);
+};
+
+var checkUserData = function() {
+	checkStory('', evalStory);
+	checkStory('', evalStory);
+	checkStory('', evalStory);
+};
+
+var evalStory = function(username, data) {
+	//console.log(data);
+	var values = JSON.parse(data);
+	console.log(values);
+	writeToChannel('I got ' + values.length + ' stories for ' + username);
+};
+
+var checkStory = function(username, callback) {
+	console.log("Checking " + username);
+	var options = {
+		host: 'hummingbird.me',
+		port: 443,
+		path: '/api/v1/users/' + username + '/feed/',
+		method: 'GET'
+	};
+
+	https.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		var body = '';
+		res.on('data', function(chunk) {
+			body += chunk;
+		});
+		res.on('end', function() {
+			callback(username, body);
+	});
+	}).end();
+};
 
 slack.on('open', function () {
 	console.log("Connection established!");
 	var channel = slack.getChannelByName('#anime');
-	channel.send('I am alive Onii-chan');
+	var general = slack.getChannelByName('#general');
+	setTimeout(checkUserData, 10000);
 });
 
 slack.on('message', function() {
